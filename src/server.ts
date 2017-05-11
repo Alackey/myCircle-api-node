10ef16ce46a1166dc547104c91a21225fc2b63e0
Anthony Lackey <alackey96@gmail.com>
2017-05-11 07:07:38 -0700

    Init commit

---
+++ src/server.ts
@@ -0,0 +1,69 @@
+/**
+ * Module dependencies.
+ */
+import * as express from "express";
+import * as compression from "compression";  // compresses requests
+import * as bodyParser from "body-parser";
+import * as logger from "morgan";
+import * as errorHandler from "errorhandler";
+import * as lusca from "lusca";
+import * as dotenv from "dotenv";
+import * as flash from "express-flash";
+import * as path from "path";
+import expressValidator = require("express-validator");
+
+
+
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
+dotenv.config({ path: ".env.example" });
+
+
+/**
+ * Controllers (route handlers).
+ */
+import * as homeController from "./controllers/home";
+
+/**
+ * Create Express server.
+ */
+const app = express();
+
+
+
+
+/**
+ * Express configuration.
+ */
+app.set("port", process.env.PORT || 3000);
+app.use(compression());
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+app.use(flash());
+app.use(lusca.xframe("SAMEORIGIN"));
+app.use(lusca.xssProtection(true));
+
+/**
+ * Primary app routes.
+ */
+app.get("/", homeController.index);
+
+
+
+/**
+ * Error Handler. Provides full stack - remove for production
+ */
+app.use(errorHandler());
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get("port"), () => {
+  console.log(("  App is running at http://localhost:%d in %s mode"), app.get("port"), app.get("env"));
+  console.log("  Press CTRL-C to stop\n");
+});
+
+module.exports = app;
